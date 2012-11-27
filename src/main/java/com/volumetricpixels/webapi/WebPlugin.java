4fb338d450f3953e50651d290091624c1838aae8
Myers Carpenter <myers@maski.org>
2012-11-26 20:39:27 -0500

    moving files so that 'mvn clean package install' will package everything correctly

--- src/main/java/com/volumetricpixels/webapi/WebPlugin.java
+++ src/main/java/com/volumetricpixels/webapi/WebPlugin.java
@@ -1,4 +1,18 @@
 /*
+ * This file is part of WebAPI.
+ *
+ * Copyright (c) 2012-2012, VolumetricPixels <http://volumetricpixels.com/>
+ * WebAPI is licensed under the VolumetricPixels License Version 1.
+ *
+ * The VolumetricPixels License is a triple license combined out of the AGPL v3
+ * License, the MIT License and the Classpath Exception License.
+ *
+ * You should have received a copy of the GNU Affero General Public License,
+ * the MIT license and the VolumetricPixels License v1 along with this program.
+ * If not, see <http://github.com/VolumetricPixels/Vitals/blob/master/License.txt>
+ * for the full license.
+ */
+/*
  * This file is subject to the terms and conditions defined in
  * file 'LICENSE.txt', which is part of this source code package.
  */
